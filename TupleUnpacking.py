3ecf35eb9386cec2e9b51b56c88775dca84ded06
Shal-coder <ltinsaebirhanu@gmail.com>
2025-04-10 09:41:56 +0300

    Tuple Unpacking

---
+++ TupleUnpacking.py
@@ -0,0 +1,9 @@
+def tuple_unpacking():
+    person = ("Sara", 20, "Jimma")
+    name, age, city = person
+    
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"City: {city}")
+
+tuple_unpacking()
