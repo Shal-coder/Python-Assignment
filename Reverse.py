c8fe0e65f9985d0f59739ccd21ccf889f1a23d42
Shal-coder <ltinsaebirhanu@gmail.com>
2025-04-10 08:34:45 +0300

    Reverse String

---
+++ Reverse.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+s=input("Enter a string:")
+print(reverse_string(s))
