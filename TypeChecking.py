ee99be3b6511da770271648e26a304e6d9679d97
Shal-coder <ltinsaebirhanu@gmail.com>
2025-04-10 09:44:38 +0300

    Type Checking

---
+++ TypeChecking.py
@@ -0,0 +1,9 @@
+x = 18
+y = 3.14
+z = "HUCISA"
+a = True
+
+print(f"Type of x: {type(x)}")     
+print(f"Type of y: {type(y)}")      
+print(f"Type of z: {type(z)}") 
+print(f"Type of a: {type(a)}")
